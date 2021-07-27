faf6255cbd9279f93de5f1f64311659bebb04b84
Murao <yoshinobu612@icloud.com>
2021-07-27 10:50:07 +0900

    start project

---
+++ Shared/BodyMeasurementApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyMeasurementApp.swift
+//  Shared
+//
+//  Created by 村尾慶伸 on 2021/07/27.
+//
+
+import SwiftUI
+
+@main
+struct BodyMeasurementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
